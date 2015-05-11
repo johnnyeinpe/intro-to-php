0f4c13a71a39a5121db485bccf07cc42d306d783
John Pe <johnpe@Johns-MacBook-Pro-2.local>
2015-05-11 23:20:20 +0200

    john added peopl.php

--- week-1/homework/people.php
+++ week-1/homework/people.php
@@ -128,7 +128,314 @@
 
 			<!-- #ToDo -->
 
-			<p>Oops, there's nothing here yet! Please check the source code.</p>
+			<p><?php
+
+			$people = array(
+		array("Name" => "September",	"Surname" => "Parker"),
+		array("Name" => "Serina",		"Surname" => "Wall"),
+		array("Name" => "Courtney",		"Surname" => "Buckley"),
+		array("Name" => "Orson",		"Surname" => "Bell"),
+		array("Name" => "Murphy",		"Surname" => "Mcintyre"),
+		array("Name" => "Brady",		"Surname" => "Wolfe"),
+		array("Name" => "Zachery",		"Surname" => "Andrews"),
+		array("Name" => "Blake",		"Surname" => "Nicholson"),
+		array("Name" => "Idola",		"Surname" => "Mullen"),
+		array("Name" => "Nora",			"Surname" => "Hansen"),
+		array("Name" => "Melodie",		"Surname" => "Pruitt"),
+		array("Name" => "Hillary",		"Surname" => "Morrison"),
+		array("Name" => "Halla",		"Surname" => "Melendez"),
+		array("Name" => "Jada",			"Surname" => "Clements"),
+		array("Name" => "Judah",		"Surname" => "Sampson"),
+		array("Name" => "Germaine",		"Surname" => "Joyce"),
+		array("Name" => "Hector",		"Surname" => "Sosa"),
+		array("Name" => "Idona",		"Surname" => "Quinn"),
+		array("Name" => "Lucian",		"Surname" => "Little"),
+		array("Name" => "Lara",			"Surname" => "Nolan"),
+		array("Name" => "Kendall",		"Surname" => "Rosa"),
+		array("Name" => "Grace",		"Surname" => "Shelton"),
+		array("Name" => "Quinn",		"Surname" => "Goodwin"),
+		array("Name" => "Brady",		"Surname" => "Alford"),
+		array("Name" => "Ramona",		"Surname" => "Alvarez"),
+		array("Name" => "Tiger",		"Surname" => "Mcgee"),
+		array("Name" => "Murphy",		"Surname" => "Contreras"),
+		array("Name" => "Imogene",		"Surname" => "Silva"),
+		array("Name" => "Matthew",		"Surname" => "Bates"),
+		array("Name" => "Addison",		"Surname" => "Collier"),
+		array("Name" => "Caryn",		"Surname" => "Keller"),
+		array("Name" => "Mikayla",		"Surname" => "Alvarado"),
+		array("Name" => "Rana",			"Surname" => "Finch"),
+		array("Name" => "Jordan",		"Surname" => "Erickson"),
+		array("Name" => "Beatrice",		"Surname" => "Coleman"),
+		array("Name" => "Macey",		"Surname" => "Atkins"),
+		array("Name" => "Kristen",		"Surname" => "Parrish"),
+		array("Name" => "Russell",		"Surname" => "Farley"),
+		array("Name" => "Lucius",		"Surname" => "Ayers"),
+		array("Name" => "Dillon",		"Surname" => "Martin"),
+		array("Name" => "Edan",			"Surname" => "Randall"),
+		array("Name" => "Shelly",		"Surname" => "West"),
+		array("Name" => "Jonah",		"Surname" => "Woods"),
+		array("Name" => "Veda",			"Surname" => "Flynn"),
+		array("Name" => "Holmes",		"Surname" => "Moran"),
+		array("Name" => "Astra",		"Surname" => "Perry"),
+		array("Name" => "Zane",			"Surname" => "Rogers"),
+		array("Name" => "Alexis",		"Surname" => "Landry"),
+		array("Name" => "Caesar",		"Surname" => "Ware"),
+		array("Name" => "Fatima",		"Surname" => "Duncan"),
+		array("Name" => "Alisa",		"Surname" => "Rojas"),
+		array("Name" => "Whilemina",	"Surname" => "Glover"),
+		array("Name" => "Rhona",		"Surname" => "Turner"),
+		array("Name" => "Doris",		"Surname" => "Dean"),
+		array("Name" => "Keane",		"Surname" => "Whitney"),
+		array("Name" => "Eagan",		"Surname" => "Blevins"),
+		array("Name" => "Ivy",			"Surname" => "Ware"),
+		array("Name" => "Dahlia",		"Surname" => "Bartlett"),
+		array("Name" => "Jessica",		"Surname" => "Bowen"),
+		array("Name" => "Barclay",		"Surname" => "Spears"),
+		array("Name" => "Graham",		"Surname" => "Andrews"),
+		array("Name" => "Carla",		"Surname" => "Mcmahon"),
+		array("Name" => "Helen",		"Surname" => "Benjamin"),
+		array("Name" => "Bell",			"Surname" => "Cleveland"),
+		array("Name" => "Baxter",		"Surname" => "Patterson"),
+		array("Name" => "Summer",		"Surname" => "Brooks"),
+		array("Name" => "Brooke",		"Surname" => "Byers"),
+		array("Name" => "Flavia",		"Surname" => "Roberson"),
+		array("Name" => "Basil",		"Surname" => "Mercer"),
+		array("Name" => "Ebony",		"Surname" => "Brock"),
+		array("Name" => "Charde",		"Surname" => "Cash"),
+		array("Name" => "Brady",		"Surname" => "Mercer"),
+		array("Name" => "Rajah",		"Surname" => "Logan"),
+		array("Name" => "Kay",			"Surname" => "Austin"),
+		array("Name" => "Declan",		"Surname" => "Kaufman"),
+		array("Name" => "Signe",		"Surname" => "Slater"),
+		array("Name" => "Fiona",		"Surname" => "Poole"),
+		array("Name" => "Mannix",		"Surname" => "Oneal"),
+		array("Name" => "Louis",		"Surname" => "Ellison"),
+		array("Name" => "Bell",			"Surname" => "Gilmore"),
+		array("Name" => "Dahlia",		"Surname" => "Mitchell"),
+		array("Name" => "Heather",		"Surname" => "Roberts"),
+		array("Name" => "Mannix",		"Surname" => "Goodman"),
+		array("Name" => "Lamar",		"Surname" => "Ellis"),
+		array("Name" => "Lionel",		"Surname" => "Bernard"),
+		array("Name" => "Jonah",		"Surname" => "May"),
+		array("Name" => "Audra",		"Surname" => "Ballard"),
+		array("Name" => "Iris",			"Surname" => "Spencer"),
+		array("Name" => "Eagan",		"Surname" => "Whitney"),
+		array("Name" => "Rafael",		"Surname" => "Cardenas"),
+		array("Name" => "Kirsten",		"Surname" => "Crane"),
+		array("Name" => "Jermaine",		"Surname" => "Patrick"),
+		array("Name" => "Ria",			"Surname" => "Farmer"),
+		array("Name" => "Demetria",		"Surname" => "Foley"),
+		array("Name" => "Tarik",		"Surname" => "Prince"),
+		array("Name" => "Driscoll",		"Surname" => "Simpson"),
+		array("Name" => "Hall",			"Surname" => "Mclaughlin"),
+		array("Name" => "Brianna",		"Surname" => "Silva"),
+		array("Name" => "Porter",		"Surname" => "Trujillo"),
+		array("Name" => "Lester",		"Surname" => "Buchanan")
+	);
+
+
+			echo $people[0]["Name"]; 
+			echo $people[0]["Surname"];
+			echo $people[1]["Name"]; 
+			echo $people[1]["Surname"];
+			echo $people[2]["Name"]; 
+			echo $people[2]["Surname"];
+			echo $people[3]["Name"]; 
+			echo $people[3]["Surname"];
+			echo $people[4]["Name"]; 
+			echo $people[4]["Surname"];
+			echo $people[5]["Name"]; 
+			echo $people[5]["Surname"];
+			echo $people[6]["Name"]; 
+			echo $people[6]["Surname"];
+			echo $people[7]["Name"]; 
+			echo $people[7]["Surname"];
+			echo $people[8]["Name"]; 
+			echo $people[8]["Surname"];
+			echo $people[9]["Name"]; 
+			echo $people[9]["Surname"];
+			echo $people[10]["Name"]; 
+			echo $people[10]["Surname"];
+			echo $people[11]["Name"]; 
+			echo $people[11]["Surname"];
+			echo $people[12]["Name"]; 
+			echo $people[12]["Surname"];
+			echo $people[13]["Name"]; 
+			echo $people[13]["Surname"];
+			echo $people[14]["Name"]; 
+			echo $people[14]["Surname"];
+			echo $people[15]["Name"]; 
+			echo $people[15]["Surname"];
+			echo $people[16]["Name"]; 
+			echo $people[16]["Surname"];
+			echo $people[17]["Name"]; 
+			echo $people[17]["Surname"];
+			echo $people[18]["Name"]; 
+			echo $people[18]["Surname"];
+			echo $people[19]["Name"]; 
+			echo $people[19]["Surname"];
+			echo $people[20]["Name"]; 
+			echo $people[20]["Surname"];
+			echo $people[21]["Name"]; 
+			echo $people[21]["Surname"];
+			echo $people[22]["Name"]; 
+			echo $people[22]["Surname"];
+			echo $people[23]["Name"]; 
+			echo $people[23]["Surname"];
+			echo $people[24]["Name"]; 
+			echo $people[24]["Surname"];
+			echo $people[25]["Name"]; 
+			echo $people[25]["Surname"];
+			echo $people[26]["Name"]; 
+			echo $people[26]["Surname"];
+			echo $people[27]["Name"]; 
+			echo $people[27]["Surname"];
+			echo $people[28]["Name"]; 
+			echo $people[28]["Surname"];
+			echo $people[29]["Name"]; 
+			echo $people[29]["Surname"];
+			echo $people[30]["Name"]; 
+			echo $people[30]["Surname"];
+			echo $people[31]["Name"]; 
+			echo $people[31]["Surname"];
+			echo $people[32]["Name"]; 
+			echo $people[32]["Surname"];
+			echo $people[33]["Name"]; 
+			echo $people[33]["Surname"];
+			echo $people[34]["Name"]; 
+			echo $people[34]["Surname"];
+			echo $people[35]["Name"]; 
+			echo $people[35]["Surname"];
+			echo $people[36]["Name"]; 
+			echo $people[36]["Surname"];
+			echo $people[37]["Name"]; 
+			echo $people[37]["Surname"];
+			echo $people[38]["Name"]; 
+			echo $people[38]["Surname"];
+			echo $people[39]["Name"]; 
+			echo $people[39]["Surname"];
+			echo $people[40]["Name"]; 
+			echo $people[40]["Surname"];
+			echo $people[41]["Name"]; 
+			echo $people[41]["Surname"];
+			echo $people[42]["Name"]; 
+			echo $people[42]["Surname"];
+			echo $people[43]["Name"]; 
+			echo $people[43]["Surname"];
+			echo $people[44]["Name"]; 
+			echo $people[44]["Surname"];
+			echo $people[45]["Name"]; 
+			echo $people[45]["Surname"];
+			echo $people[46]["Name"]; 
+			echo $people[46]["Surname"];
+			echo $people[47]["Name"]; 
+			echo $people[47]["Surname"];
+			echo $people[48]["Name"]; 
+			echo $people[48]["Surname"];
+			echo $people[49]["Name"]; 
+			echo $people[49]["Surname"];
+			echo $people[50]["Name"]; 
+			echo $people[50]["Surname"];
+			echo $people[51]["Name"]; 
+			echo $people[51]["Surname"];
+			echo $people[52]["Name"]; 
+			echo $people[52]["Surname"];
+			echo $people[53]["Name"]; 
+			echo $people[53]["Surname"];
+			echo $people[54]["Name"]; 
+			echo $people[54]["Surname"];
+			echo $people[55]["Name"]; 
+			echo $people[55]["Surname"];
+			echo $people[56]["Name"]; 
+			echo $people[56]["Surname"];
+			echo $people[57]["Name"]; 
+			echo $people[57]["Surname"];
+			echo $people[58]["Name"]; 
+			echo $people[58]["Surname"];
+			echo $people[59]["Name"]; 
+			echo $people[59]["Surname"];
+			echo $people[60]["Name"]; 
+			echo $people[60]["Surname"];
+			echo $people[61]["Name"]; 
+			echo $people[61]["Surname"];
+			echo $people[62]["Name"]; 
+			echo $people[62]["Surname"];
+			echo $people[63]["Name"]; 
+			echo $people[63]["Surname"];
+			echo $people[64]["Name"]; 
+			echo $people[64]["Surname"];
+			echo $people[65]["Name"]; 
+			echo $people[65]["Surname"];
+			echo $people[66]["Name"]; 
+			echo $people[66]["Surname"];
+			echo $people[67]["Name"]; 
+			echo $people[67]["Surname"];
+			echo $people[68]["Name"]; 
+			echo $people[68]["Surname"];
+			echo $people[69]["Name"]; 
+			echo $people[69]["Surname"];
+			echo $people[70]["Name"]; 
+			echo $people[70]["Surname"];
+			echo $people[71]["Name"]; 
+			echo $people[71]["Surname"];
+			echo $people[72]["Name"]; 
+			echo $people[72]["Surname"];
+			echo $people[73]["Name"]; 
+			echo $people[73]["Surname"];
+			echo $people[74]["Name"]; 
+			echo $people[74]["Surname"];
+			echo $people[75]["Name"]; 
+			echo $people[75]["Surname"];
+			echo $people[76]["Name"]; 
+			echo $people[76]["Surname"];
+			echo $people[77]["Name"]; 
+			echo $people[77]["Surname"];
+			echo $people[78]["Name"]; 
+			echo $people[78]["Surname"];
+			echo $people[79]["Name"]; 
+			echo $people[79]["Surname"];
+			echo $people[80]["Name"]; 
+			echo $people[80]["Surname"];
+			echo $people[81]["Name"]; 
+			echo $people[81]["Surname"];
+			echo $people[82]["Name"]; 
+			echo $people[82]["Surname"];
+			echo $people[83]["Name"]; 
+			echo $people[83]["Surname"];
+			echo $people[84]["Name"]; 
+			echo $people[84]["Surname"];
+			echo $people[85]["Name"]; 
+			echo $people[85]["Surname"];
+			echo $people[86]["Name"]; 
+			echo $people[86]["Surname"];
+			echo $people[87]["Name"]; 
+			echo $people[87]["Surname"];
+			echo $people[88]["Name"]; 
+			echo $people[88]["Surname"];
+			echo $people[89]["Name"]; 
+			echo $people[89]["Surname"];
+			echo $people[90]["Name"]; 
+			echo $people[90]["Surname"];
+			echo $people[91]["Name"]; 
+			echo $people[91]["Surname"];
+			echo $people[92]["Name"]; 
+			echo $people[92]["Surname"];
+			echo $people[93]["Name"]; 
+			echo $people[93]["Surname"];
+			echo $people[94]["Name"]; 
+			echo $people[94]["Surname"];
+			echo $people[95]["Name"]; 
+			echo $people[95]["Surname"];
+			echo $people[96]["Name"]; 
+			echo $people[96]["Surname"];
+			echo $people[97]["Name"]; 
+			echo $people[97]["Surname"];
+			echo $people[98]["Name"]; 
+			echo $people[98]["Surname"];
+			echo $people[99]["Name"]; 
+			echo $people[99]["Surname"];
+			
+				?></p>
 
 		</div>
 
